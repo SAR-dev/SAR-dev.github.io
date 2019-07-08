7b2a87c961b39be49302dfd6e5588b3e2954dbef
SAR-dev <51741328+SAR-dev@users.noreply.github.com>
2019-07-08 13:26:19 +0600

    up

--- js/script.js
+++
@@ -1,5 +0,0 @@
-import commentBox from 'commentbox.io';
-// or
-const commentBox = require('commentbox.io');
-// or if using the CDN, it will be available as a global "commentBox" variable.
-commentBox('5667711371706368-proj');
